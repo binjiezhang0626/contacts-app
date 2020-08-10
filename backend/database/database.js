9eeb47ecaca6465e50651dad0a4c2f317337d6e8
Binjie <im.binjie.zhang@outlook.com>
2020-08-10 21:06:33 +0800

    fix: database connection issue

--- backend/database/database.js
+++ backend/database/database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const mysql = require('mysql');
 
 // MySQL Configuration
-const mysqlConnectionPool = mysql.createConnection(`${process.env.CLEARDB_DATABASE_URL}&connectionLimit=10`);
+const mysqlConnectionPool = mysql.createPool(`${process.env.CLEARDB_DATABASE_URL}&connectionLimit=10`);
 
 const queryWithPromise = (queryStatement, queryInput = []) => new Promise(
   (resolve, reject) => {
